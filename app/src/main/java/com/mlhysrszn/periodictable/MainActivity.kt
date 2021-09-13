bedacbd5e1a4ade9b9c623070842406e7bd1f5f3
Melih Yaşar SÖZEN <mlhysrszn@gmail.com>
2021-09-14 02:45:50 +0300

    Model class added

--- app/src/main/java/com/mlhysrszn/periodictable/MainActivity.kt
+++ app/src/main/java/com/mlhysrszn/periodictable/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.mlhysrszn.periodictable
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.mlhysrszn.periodictable.databinding.ActivityMainBinding
+import com.mlhysrszn.periodictable.model.Element
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+//        val element = Element(6, "C", "Carbon", 12.011f, "[He] 2s2 2p2", R.color.black, "Img")
+//        val emptyElement = Element()
     }
 }
